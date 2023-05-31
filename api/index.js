474726363c83249e992fcb4ce3edbb8a486ac100
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-31 16:50:20 -0300

    recuperando dados ao pressionar register

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.post('/register',(req,res)=>{
-  res.json('test ok')
+  const {username,password} = req.body
+  res.json({requestData:{username,password}})
   
 })
 
