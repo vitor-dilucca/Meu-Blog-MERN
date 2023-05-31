0f45b43e78230c59e82bfd992c22bb4050be31ff
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-31 17:22:37 -0300

    fui no mongodb criei o projeto novo, o cluster, peguei o link do cluster e fiz a conexao no index.js com mongoose.connect(link) criei o model do User com seu Schema e fiz o codigo p criar o usuario novo no index

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/User')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/register',(req,res)=>{
+mongoose.connect('mongodb+srv://blog:D3cSJt5CHFjuPfkJ@cluster0.zbpaovy.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req,res)=>{
   const {username,password} = req.body
-  res.json({requestData:{username,password}})
-  
+  const userDoc = await User.create({
+    username,password
+  })
+  // res.json({requestData:{username,password}})
+  res.json(userDoc)
 })
 
 app.listen(4000)
