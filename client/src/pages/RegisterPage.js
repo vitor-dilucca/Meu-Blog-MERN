6b7b0ebd0d58f44a909f4dbb06e5fb8033f4f2fc
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-31 17:43:37 -0300

    aparece um alertzin dizendo se o registro deu certo ou nao

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -7,12 +7,16 @@ export default function RegisterPage() {
 
   async function register(ev) {
     ev.preventDefault()
-     await fetch('http://localhost:4000/register',{
+    const response = await fetch('http://localhost:4000/register',{
       method:'POST',
       body: JSON.stringify({username,password}),
       headers:{'Content-Type':'application/json'}
     })
-    // console.log(response);
+    if(response.status === 200){
+      alert('register successful')
+    }else{
+      alert('register failed')
+    }
   }
 
   return (
