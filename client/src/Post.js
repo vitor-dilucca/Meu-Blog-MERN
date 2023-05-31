f696617a94b87f82ad70b015dfaf16c415b8f4e7
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-31 13:35:18 -0300

    componentizando o post e o header, e instalei um modulo na pasta raiz acho q n ta certo mas ok

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+  return(
+    <div className="post">
+        <div className="image">
+          <img src="https://integralismo.org.br/wp-content/uploads/2022/09/Independencia-ou-morte-integralismo-768x411.jpeg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>A verdadeira história da Independência</h2>
+          <p className="info">
+            <a href="" className="author">Dawid Paszko</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">A Independência foi o resultado de séculos de história de uma nação, e foi conquistada pelo povo</p>
+        </div>
+      </div>
+  )
+}
