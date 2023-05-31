01820b8011a0873e9ce2930e1cf91b2ebd50bb60
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-31 15:02:57 -0300

    msm coisa

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,7 @@
 export default function LoginPage() {
   return (
     <form className="login" action="">
-            <h1>Login</h1>
+      <h1>Login</h1>
 
       <input type="text" placeholder="username" name="" id="" />
       <input type="password" placeholder="password" name="" id="" />
