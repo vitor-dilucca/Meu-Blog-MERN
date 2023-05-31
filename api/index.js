3016a93df22357c323a601746e0690ce2c47ecc5
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-31 17:49:19 -0300

    encriptando a senha usando lib 'bcrypt'

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/User')
+const bcrypt = require('bcryptjs')
 const app = express()
+
+const salt = bcrypt.genSaltSync(10)
 
 app.use(cors())
 app.use(express.json())
@@ -13,12 +16,14 @@ app.post('/register', async (req,res)=>{
   const {username,password} = req.body
   try{
     const userDoc = await User.create({
-      username,password
+      username,
+      password:bcrypt.hashSync(password,salt)
     })
     // res.json({requestData:{username,password}})
     res.json(userDoc)
   }catch(e){
     res.status(400).json(e)
+    console.log(e)
   }
 })
 
