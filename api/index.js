487ddaddb515e0e0d403ea6be02ac2732e33b329
Vitor Dilucca <vitordilucca@hotmail.com>
2023-05-31 21:19:22 -0300

    baixado cookie-parser, feito uma alteraçao no component Header para caso o login for feito e tiver o token tira aqueles botao de login e register mas ta incompleto ainda

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User')
 const bcrypt = require('bcryptjs')
 const app = express()
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 const salt = bcrypt.genSaltSync(10)
 const secret = 'çlknqmçklqwue3223525'
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}))
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect('mongodb+srv://blog:D3cSJt5CHFjuPfkJ@cluster0.zbpaovy.mongodb.net/?retryWrites=true&w=majority')
 
@@ -41,6 +43,14 @@ app.post('/login', async (req, res) => {
     res.status(400).json('wrong credentials')
   }
   console.log({passOk})
+})
+
+app.get('/profile',(req,res)=>{
+  const {token} =req.cookies
+  jwt.verify(token,secret,{},(err,info)=>{
+    if(err) throw err
+    res.json(info)
+  })
 })
 
 app.listen(4000)
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,6 +1,12 @@
 import {Link} from 'react-router-dom'
+import {useEffect} from 'react'
 
 export default function Header(){
+  useEffect(()=>{
+    fetch('http://localhost:4000/profile',{
+      credentials:'include'
+    })
+  },[])
   return(
     <header>
         <Link to="/" className="logo">MeuBlog</Link>
