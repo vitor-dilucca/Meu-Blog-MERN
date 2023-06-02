04af4e67eac4fa2f8967c47df4bc0c1d18091769
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-02 18:30:27 -0300

    enviando a imagem e os dados do novo post para o endpoint, o endpoint do /createpost ainda n foi feito

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -6,6 +6,20 @@ export default function CreatePost() {
   const [title, setTitle] = useState('')
   const [summary, setSummary] = useState('')
   const [content, setContent] = useState('')
+  const [files, setFiles] = useState('')
+  function createNewPost(ev) {
+    const data = new FormData()
+    data.set('title', title)
+    data.set('summary', summary)
+    data.set('content', content)
+    data.set('file', files[0])
+    console.log(files);
+    ev.preventDefault()
+    fetch('http://localhost:4000/post',{
+      method:'POST',
+      body: data
+    })
+  }
   const modules = {
     toolbar: [
       [{ 'header': [1, 2, false] }],
@@ -33,12 +47,13 @@ export default function CreatePost() {
         placeholder={'Summary'}
         value={summary}
         onChange={ev => setSummary(ev.target.value)} />
-      <input type="file" />
+      <input type="file"
+        onChange={ev => setFiles(ev.target.files)} />
       <ReactQuill
         value={content}
         onChange={newValue => setContent(newValue)}
         modules={modules} formats={formats} />
-      <button style={{ marginTop: '5px' }}>Create post</button>
+      <button onClick={createNewPost} style={{ marginTop: '5px' }}>Create post</button>
     </form>
   )
 }
