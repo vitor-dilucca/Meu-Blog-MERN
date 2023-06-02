d9399ded8d66646c6496d8fb8df5c414913ff881
Vitor Dilucca <vitordilucca@hotmail.com>
2023-06-02 19:13:27 -0300

    mandando os dados do post pro MongoDB sucesfuly

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/User')
+const Post = require('./models/Post')
 const bcrypt = require('bcryptjs')
 const app = express()
 const jwt = require('jsonwebtoken')
@@ -63,13 +64,21 @@ app.post('/logout', (req, res) => {
   res.cookie('token', '').json('ok')
 })
 
-app.post('/post', uploadMiddleware.single('file'), (req, res) => {
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
   const { originalname, path } = req.file
   const parts = originalname.split('.')
   const ext = parts[parts.length - 1]
-  fs.renameSync(path, path + '.' + ext)
-  res.json({ ext })
-  // res.json({files:req.file})
+  const newPath = path + '.' + ext
+  fs.renameSync(path, newPath)
+
+  const {title, summary, content} = req.body
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover:newPath,
+  })
+  res.json(postDoc)
 })
 
 app.listen(4000)
